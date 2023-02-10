f345e91f8f02a314d643491e1386446a1ea402aa
yudur <duarteyudi218@gmail.com>
2023-02-10 16:17:26 -0300

    add follow function

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from selenium.webdriver import Firefox
+from selenium.webdriver import Firefox, DesiredCapabilities
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.by import By
+
+# from selenium.webdriver.common.proxy import Proxy, ProxyType
+
+import pyautogui
 
 import time
 
@@ -8,6 +12,17 @@ import os
 import dotenv
 
 dotenv.load_dotenv()  # loading the env
+
+# TODO: proxy apenas se nescessaria
+# myProxy = "20.111.54.16:80"
+
+# proxy = Proxy({
+#     'proxyType': ProxyType.MANUAL,
+#     'httpProxy': myProxy,
+#     'ftpProxy': myProxy,
+#     'sslProxy': myProxy,
+#     'noProxy': '' # set this value as desired
+#     })
 
 
 # class responsible for the logic of the program 
@@ -51,7 +66,21 @@ class FirefoxDriver:
             except:
                 running = False
 
-    def login_twitter(self):
+    def __send_password(self) -> None:
+        self.driver.implicitly_wait(10)
+        
+        self.driver.find_element(
+            By.XPATH,
+            "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input"
+        ).send_keys(os.getenv("PASSWORD_TWITTER"))
+
+        self.driver.find_element(
+            By.XPATH,
+            "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div"
+        ).click()
+
+    # log in to twitter
+    def login_twitter(self) -> None:
         # get current window handle
         current_tab = self.driver.current_window_handle
         # get first child window
@@ -63,35 +92,53 @@ class FirefoxDriver:
                 self.driver.switch_to.window(w)
                 break
         
-        self.driver.maximize_window()
+        # self.driver.maximize_window()
 
-        self.driver.implicitly_wait(15)
+        self.driver.implicitly_wait(25)
         self.driver.find_element(
             By.XPATH,
             "/html/body/div[1]/div/div/div[1]/div[2]/div[1]/div/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/span/span/span"
         ).click()
 
-        self.driver.implicitly_wait(15)
+        self.driver.implicitly_wait(25)
         self.driver.find_element(
             By.XPATH,
             "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input"
         ).send_keys(os.getenv("EMAIL_TWITTER"))
-
+        
+        self.driver.implicitly_wait(25)
         self.driver.find_element(
             By.XPATH,
             "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div"
         ).click()
 
-        self.driver.implicitly_wait(10)
-        self.driver.find_element(
-            By.XPATH,
-            "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input"
-        ).send_keys(os.getenv("PASSWORD_TWITTER"))
 
-        self.driver.find_element(
-            By.XPATH,
-            "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div"
-        ).click()
+        try:
+            self.__send_password()
+        except:
+            self.driver.implicitly_wait(25)
+            self.driver.find_element(
+                By.XPATH,
+                "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input"
+            ).send_keys(os.getenv("USERNAME_TWITTER"))
+
+            self.driver.find_element(
+                By.XPATH,
+                "/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div"
+            ).click()
+
+            self.__send_password()
+
+    def follow(self) -> None:
+        running = True
+        while running:
+            try:
+                time.sleep(1)
+                imgButton = pyautogui.locateCenterOnScreen("images/program/followButton1.png", confidence=0.7)  # type: ignore
+                pyautogui.click(imgButton.x, imgButton.y)
+                running = False
+            except:
+                continue
 
     # main function
     def main(self) -> None:
@@ -119,6 +166,15 @@ class FirefoxDriver:
         ).click()
 
         self.login_twitter()
+        self.follow()
+
+        # confirm that you followed
+        
+        # self.driver.implicitly_wait(8)
+        # self.driver.find_element(
+            # By.XPATH,
+            # "/html/body/div[6]/div/div[1]/div[2]/div[2]/div[4]/div[1]/div[2]/div[1]/div/div[1]/a"
+        # ).click()
 
 
 if __name__ == "__main__":
